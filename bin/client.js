a574b66f6482e25074c01fd942791814364a9653
ChalkPE <chalk@chalk.pe>
2017-05-15 22:48:13 +0900

    Reformatted code

--- bin/client.js
+++ bin/client.js
@@ -3,130 +3,181 @@
  * @since 2016-07-07
  */
 
-var blessed = require('blessed');
+var blessed = require('blessed')
 
 var screen = blessed.screen({
-    smartCSR: true,
-    dockBorders: true,
-    fullUnicode: true,
-    title: 'Hearthstone',
+  smartCSR: true,
+  dockBorders: true,
+  fullUnicode: true,
+  title: 'Hearthstone',
 
-    cursor: {
-        artificial: true,
-        shape: 'underline',
-        blink: true
-    }
-});
+  cursor: {
+    artificial: true,
+    shape: 'underline',
+    blink: true
+  }
+})
 
-screen.on('destroy', () => process.exit(0));
+screen.on('destroy', () => process.exit(0))
 
 var logger = blessed.log({
-    parent: screen,
-    width: '100%', height: '100%-2', top: 0, left: 0,
-    padding: { top: 0, left: 1, bottom: 0, right: 1 },
+  parent: screen,
 
-    tags: true, keys: true, mouse: true, hidden: true,
-    border: 'line', label: ' {blue-fg}Messages{/} ',
+  width: '100%',
+  height: '100%-2',
 
-    style: { fg: 'white' },
-    scrollable: true, scrollback: 512
-});
+  top: 0,
+  left: 0,
+  padding: { top: 0, left: 1, bottom: 0, right: 1 },
+
+  tags: true,
+  keys: true,
+  mouse: true,
+  hidden: true,
+
+  border: 'line',
+  label: ' {blue-fg}Messages{/} ',
+  style: { fg: 'white' },
+
+  scrollable: true,
+  scrollback: 512
+})
 
 var commandTextbox = blessed.textbox({
-    parent: screen,
-    width: '100%', height: 3, bottom: 0, left: 0,
-    padding: { top: 0, left: 1, bottom: 0, right: 1 },
+  parent: screen,
 
-    tags: true, keys: true, mouse: true, hidden: true,
-    border: 'line', style: { fg: 'yellow' }, inputOnFocus: true
-});
+  width: '100%',
+  height: 3,
 
-commandTextbox.on('submit', onCommandAreaSubmit);
+  bottom: 0,
+  left: 0,
+  padding: { top: 0, left: 1, bottom: 0, right: 1 },
+
+  tags: true,
+  keys: true,
+  mouse: true,
+  hidden: true,
+
+  border: 'line',
+  style: { fg: 'yellow' },
+  inputOnFocus: true
+})
+
+commandTextbox.on('submit', onCommandAreaSubmit)
 
 var prompt = blessed.prompt({
-    parent: screen,
-    width: 'half', height: 'shrink', top: 'center', left: 'center',
-    padding: { top: 1, left: 2, bottom: 1, right: 2 },
+  parent: screen,
 
-    tags: true, keys: true, mouse: true,
-    border: 'line', label: ' {blue-fg}Hearthstone{/} '
-});
+  width: 'half',
+  height: 'shrink',
+
+  top: 'center',
+  left: 'center',
+  padding: { top: 1, left: 2, bottom: 1, right: 2 },
+
+  tags: true,
+  keys: true,
+  mouse: true,
+
+  border: 'line',
+  label: ' {blue-fg}Hearthstone{/} '
+})
 
 var message = blessed.message({
-    parent: screen,
-    width: 'shrink', height: 'shrink', top: 'center', left: 'center',
-    padding: { top: 1, left: 2, bottom: 1, right: 2 },
+  parent: screen,
 
-    tags: true, keys: true, mouse: true, hidden: true,
-    border: 'line', label: ' {blue-fg}Hearthstone{/} '
-});
+  width: 'shrink',
+  height: 'shrink',
+
+  top: 'center',
+  left: 'center',
+  padding: { top: 1, left: 2, bottom: 1, right: 2 },
+
+  tags: true,
+  keys: true,
+  mouse: true,
+  hidden: true,
+
+  border: 'line',
+  label: ' {blue-fg}Hearthstone{/} '
+})
 
 var loading = blessed.loading({
-    parent: screen,
-    width: 'shrink', height: 'shrink', top: 'center', left: 'center',
-    padding: { top: 1, left: 2, bottom: 1, right: 2 },
+  parent: screen,
 
-    tags: true, keys: true, mouse: true, hidden: true,
-    border: 'line', label: ' {blue-fg}Hearthstone{/} '
-});
+  width: 'shrink',
+  height: 'shrink',
 
-screen.key('C-c', () => screen.destroy());
-screen.render();
+  top: 'center',
+  left: 'center',
+  padding: { top: 1, left: 2, bottom: 1, right: 2 },
 
-var io = require('socket.io-client');
-var socket, username, hostname;
+  tags: true,
+  keys: true,
+  mouse: true,
+  hidden: true,
 
-function initialize(){
-    prompt.input('{yellow-fg}Address: {/}', (error, address) => {
-        if(!address) return screen.destroy();
-        if(!address.includes('@')) return message.error('The address must be {bold}username@hostname{/}.', initialize);
+  border: 'line',
+  label: ' {blue-fg}Hearthstone{/} '
+})
 
-        [username, hostname] = address.split('@').map(str => str.trim());
+screen.key('C-c', () => screen.destroy())
+screen.render()
 
-        loading.load(`Connecting to ${hostname}...`);
-        socket = io(`http://${hostname}:10413`);
+var io = require('socket.io-client')
+var socket, username, hostname
 
-        socket.on('connect', onConnect);
-        socket.on('hello',   onHello);
-        socket.on('command', onCommand);
-        socket.on('message', onMessage);
-    });
+function initialize () {
+  prompt.input('{yellow-fg}Address: {/}', (err, address) => {
+    if (err || !address) return screen.destroy()
+    if (!address.includes('@')) return message.error('The address must be {bold}username@hostname{/}.', initialize);
+
+    [username, hostname] = address.split('@').map(str => str.trim())
+
+    loading.load(`Connecting to ${hostname}...`)
+    socket = io(`http://${hostname}:10413`)
+
+    socket.on('connect', onConnect)
+    socket.on('hello', onHello)
+    socket.on('command', onCommand)
+    socket.on('message', onMessage)
+  })
 }
 
-function onCommandAreaSubmit(command){
-    if(command && (command = command.trim()).length > 1){
-        if(command.toLowerCase() === '/exit') return screen.destroy();
-        socket.emit(command.startsWith('/') ? 'command' : 'message', command);
-    }
+function onCommandAreaSubmit (command) {
+  if (command && (command = command.trim()).length > 1) {
+    if (command.toLowerCase() === '/exit') return screen.destroy()
+    socket.emit(command.startsWith('/') ? 'command' : 'message', command)
+  }
 
-    commandTextbox.clearInput();
-    commandTextbox.focus();
-    screen.render();
+  commandTextbox.clearInput()
+  commandTextbox.focus()
+  screen.render()
 }
 
-function onConnect(){
-    socket.emit('hello', username);
+function onConnect () {
+  socket.emit('hello', username)
 }
 
-function onHello(res){
-    loading.stop();
-    if(!res.ok) return message.error(res.message, initialize);
+function onHello (res) {
+  loading.stop()
+  if (!res.ok) return message.error(res.message, initialize)
 
-    screen.title = `Hearthstone: ${username}@${hostname}`;
+  screen.title = `Hearthstone: ${username}@${hostname}`
 
-    logger.show();
-    commandTextbox.show();
-    commandTextbox.focus();
+  logger.show()
+  commandTextbox.show()
+  commandTextbox.focus()
 
-    logger.log(res.message);
+  logger.log(res.message)
 }
 
-function onCommand(res){
-    logger.log(`{yellow-fg}${res}{/}`);
+function onCommand (res) {
+  logger.log(`{yellow-fg}${res}{/}`)
 }
 
-function onMessage(msg){
-    logger.log(msg);
+function onMessage (msg) {
+  logger.log(msg)
 }
 
-initialize();
+initialize()
--- bin/server.js
+++ bin/server.js
@@ -3,27 +3,27 @@
  * @since 2016-07-07
  */
 
-const io = require("socket.io")(10413);
+const io = require('socket.io')(process.env.PORT || 10413)
 const sockets = {
-    data: new Map(),  //socket.client.id -> object { username, ...etc }
-    names: new Map(), //string -> socket
-    online: {
-        [Symbol.iterator]: function*(){
-            for(let data of sockets.data.values()) yield data.username;
-        }
+  data: new Map(),  // socket.client.id -> object { username, ...etc }
+  names: new Map(), // string -> socket
+  online: {
+    [Symbol.iterator]: function * () {
+      for (let data of sockets.data.values()) yield data.username
     }
-};
+  }
+}
 
-const hello = require('../lib/server/socket/hello')(io, sockets);
-const message = require('../lib/server/socket/message')(io, sockets);
-const command = require('../lib/server/socket/command')(io, sockets);
-const disconnect = require('../lib/server/socket/disconnect')(io, sockets);
+const hello = require('../lib/server/socket/hello')(io, sockets)
+const message = require('../lib/server/socket/message')(io, sockets)
+const command = require('../lib/server/socket/command')(io, sockets)
+const disconnect = require('../lib/server/socket/disconnect')(io, sockets)
 
 io.on('connection', socket => {
-    console.log('conne:', socket.client.id);
+  console.log('conne:', socket.client.id)
 
-    socket.on('hello', hello(socket));
-    socket.on('message', message(socket));
-    socket.on('command', command(socket));
-    socket.on('disconnect', disconnect(socket));
-});
+  socket.on('hello', hello(socket))
+  socket.on('message', message(socket))
+  socket.on('command', command(socket))
+  socket.on('disconnect', disconnect(socket))
+})
