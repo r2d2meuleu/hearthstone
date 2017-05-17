57727c36f9e28baca17bd3c8440d79529b204dca
ChalkPE <chalk@chalk.pe>
2017-05-17 23:04:53 +0900

    Updated Card model

--- bin/server.js
+++ bin/server.js
@@ -3,7 +3,11 @@
  * @since 2016-07-07
  */
 
-const io = require('socket.io')(process.env.PORT || 10413)
+const socketIo = require('socket.io')
+const init = require('../lib/server/init')
+const connection = require('../lib/server/socket')
+
+const io = socketIo(process.env.PORT || 10413)
 const sockets = {
   names: new Map(), // string -> socket
   data: new Map(),  // socket.client.id -> object
@@ -15,12 +19,6 @@ const sockets = {
   }
 }
 
-const database = require('../lib/server/database')
-const connection = require('../lib/server/socket')
-
-const Card = require('../lib/server/models/Card')
-
-database().then(async cards => {
-  (await cards.find({})).forEach(x => console.log(new Card(x).toString()))
-  io.on('connection', connection(io, sockets))
-})
+init()
+  .then(() => io.on('connection', connection(io, sockets)))
+  .catch(err => console.error(err))
